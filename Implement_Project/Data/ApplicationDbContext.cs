d67bbd48ead351deb3b7734cc2f654aafdb5003c
MBoota <m43577535@gmail.com>
2023-10-27 23:09:09 +0500

    Add project files.

---
+++ Implement_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Implement_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Implement_Project.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
