1a694244b969041983a31573b292bc393ec75745
MBoota <m43577535@gmail.com>
2023-10-28 10:44:40 +0500

    dataSeeding in for Dummy Records

--- Implement_Project/Data/ApplicationDbContext.cs
+++ Implement_Project/Data/ApplicationDbContext.cs
@@ -10,5 +10,20 @@ namespace Implement_Project.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+
+
+        //__________________ 1. Data Seeding ____________________________
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1 , Name ="Mobile" , DisplayOrder = 1},
+                new Category { CategoryId = 2 , Name ="Fruites" , DisplayOrder = 2},
+                new Category { CategoryId = 3 , Name ="Laptops" , DisplayOrder = 2},
+                new Category { CategoryId = 4 , Name ="Electronics" , DisplayOrder = 2},
+                new Category { CategoryId = 5 , Name ="Dry Fruites" , DisplayOrder = 2},
+                new Category { CategoryId = 6 , Name ="Pots" , DisplayOrder = 2},
+                new Category { CategoryId = 7 , Name ="Vegitabes" , DisplayOrder = 2}
+            );
+        }
     }
 }
